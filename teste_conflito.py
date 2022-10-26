e3f06ff7a3453919e98e1c13f02ff5f33b633875
Walkiria Resende <walkiriaresende@gmail.com>
2022-10-26 20:03:08 -0300

    Update teste_conflito.py

--- teste_conflito.py
+++ teste_conflito.py
@@ -13,32 +13,5 @@ import pandas as pd
 print("hello world")
 
 for i in range(0, 50):
-    if i < 30:
-        print(i*1000)
-    else:
-        print(i*5)
-
-
-frutas = ["banana", "ameixa"]
-
-dic = {}
-
-print(dic)
-
-print("meu nome é Igor")
-
     
-
-
-
-
-for i in range(0,10):
-    print(i)
-
-dicionario = {}
-dicionario['chave'] = ['valor', 'valor1']
-
-dicionario2={}
-dicionario2['chave'] = ['valor', 'valor1']
-
 
