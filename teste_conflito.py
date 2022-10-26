d23a7a5a233aaaa8d14541c33c39ff82c4a8c3e8
Igornegrao <Igornegrao1@gmail.com>
2022-10-26 19:47:01 -0300

    Update teste_conflito.py

--- teste_conflito.py
+++ teste_conflito.py
@@ -10,6 +10,27 @@ import pandas as pd
 # In[2]:
 
 
+print("hello world")
+
+for i in range(0, 50):
+    if i < 30:
+        print(i*10)
+    else:
+        print(i*5)
+
+
+frutas = ["banana", "maça", "ameixa"]
+pessoas = ["Walkiria", "Igor", "Baronão"]
+
+dic = {}
+
+print(dic)
+
+    
+
+
+
+
 for i in range(0,10):
     print(i)
 
